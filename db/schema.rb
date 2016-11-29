dce72f409f4f263ddcf1347c704469ebc29cc69e
Julie Pierre <julie.pierre3@gmail.com>
2016-11-29 12:28:23 +0100

    completed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161128174742) do
+ActiveRecord::Schema.define(version: 20161129112737) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -84,9 +84,9 @@ ActiveRecord::Schema.define(version: 20161128174742) do
     t.float    "lng"
     t.text     "description"
     t.integer  "user_id"
-    t.boolean  "accepted"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.boolean  "accepted",    default: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
     t.index ["harbor_id"], name: "index_spots_on_harbor_id", using: :btree
     t.index ["user_id"], name: "index_spots_on_user_id", using: :btree
   end
@@ -102,19 +102,19 @@ ActiveRecord::Schema.define(version: 20161128174742) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.boolean  "ambassador"
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
+    t.boolean  "ambassador",             default: false
     t.integer  "weight"
     t.string   "first_name"
     t.string   "last_name"
