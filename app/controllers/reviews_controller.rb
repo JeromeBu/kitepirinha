f61f77a625428fe208e7e2735ced472637942ffe
Julie Pierre <julie.pierre3@gmail.com>
2016-11-29 13:09:56 +0100

    WIP

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,21 @@
 class ReviewsController < ApplicationController
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @spot = Spot.find(params[spot_id])
+    @review = Review.new(review_params)
+    @review.spot = @spot
+    @review.user = current_user
+    @review.save
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+
 end
