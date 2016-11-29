daa6810c2d6b94bacbd2f64fceb35c085b164d01
Jeremy ISRAEL <israel.jeremy@gmail.com>
2016-11-29 11:28:58 +0100

    fixed weather feedback controller name in routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   resources :favorite_spots, only: [ :create, :index, :destroy ]
   resources :spots do
     resources :reviews, only: [ :create ]
-    resources :feedbacks, only: [ :new, :create ]
+    resources :weather_feedbacks, only: [ :new, :create ]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
