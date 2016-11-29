2af3e92307a410ae6a4b2bf2d22afdbad55ec4bb
Jerome BURKARD <jerome.burkard@gadz.org>
2016-11-29 17:45:34 +0100

    spots methode for fresh data and api calling moved in spot.rb

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -1,13 +1,9 @@
-require "json"
-require "open-uri"
-
 class SpotsController < ApplicationController
   before_action :set_spot, only: [:show, :edit, :update]
   skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
     @spots = policy_scope(Spot)
-    # forecast_data(Spot.first)
   end
 
   def show
@@ -27,6 +23,7 @@ class SpotsController < ApplicationController
     else
       render :new
     end
+    @spot.fresh_forecasts
   end
 
   def edit
@@ -55,49 +52,5 @@ class SpotsController < ApplicationController
 
   def spot_params
     require.params(:spot).permit(:description, :lat, :lng, :user_id)
-  end
-
-  def forecast_data(spot)
-  # Testing if the spot last data is older then 2 hours
-    most_recent_date = 0
-    spot.forecasts.each do |forecast|
-      most_recent_date = forecast.created_at if forecast.created_at > most_recent_date
-    end
-
-    if DateTime.now < most_recent_date + 2.hours
-      Forecast.where("created_at > ?", most_recent_date - 1.minutes).where(spot: spot)
-    else
-      parse_forecast_data(spot)
-      # code ci dessous pas DRY, on pourrait tenter un truc récursif en rappelant la fonction forecast_data
-      spot.forecasts.each do |forecast|
-        most_recent_date = forecast.created_at if forecast.created_at > most_recent_date
-      end
-      Forecast.where("created_at > ?", most_recent_date - 2.minutes).where(spot: spot)
-      # fin du pas très DRY
-    end
-  end
-
-  def parse_forecast_data(spot)
-    # Calling darksky api and saving in DB
-    lat = spot.lat
-    lng = spot.lng
-    url = open("https://api.darksky.net/forecast/#{ENV['DARKSKY_API_KEY']}/#{lat},#{lng}?units=ca").read
-    forecast_raw_json = JSON.parse(url)
-
-    hourly = forecast_raw_json["hourly"]["data"]
-
-    hourly.each do |data_for_the_hour|
-      forecast = Forecast.new({
-        spot: spot,
-        date_time: DateTime.strptime(data_for_the_hour["time"].to_s, '%s'),
-        wind_strength: (data_for_the_hour["windSpeed"]/1.852), # vitesse du vent enregistrée en kt
-        wind_direction: data_for_the_hour["windBearing"],
-        precip_intensity: data_for_the_hour["precipIntensity"],
-        precip_probability: data_for_the_hour["precipProbability"],
-        cloud_cover: data_for_the_hour["cloudCover"],
-        temperature: data_for_the_hour["temperature"]
-      })
-      forecast.save
-    end
   end
 end
--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,3 +1,6 @@
+require "json"
+require "open-uri"
+
 class Spot < ApplicationRecord
   belongs_to :harbor
   belongs_to :user
@@ -14,4 +17,53 @@ class Spot < ApplicationRecord
   validates :lat, presence: true
   validates :lng, presence: true
   validates :description, presence: true
+
+  def fresh_forecasts
+    #Retourne les forecasts de moins de 2H en allant les chercher si nécessaire
+
+
+  # Testing if the spot last data is older then 2 hours
+    most_recent_date = 0
+    self.forecasts.each do |forecast|
+      most_recent_date = forecast.created_at if forecast.created_at > most_recent_date
+    end
+    # optimisation possible : reduire la table pour ne boucler que sur les derniers forecasts
+    if DateTime.now < most_recent_date + 2.hours
+      @fresh_forecasts = Forecast.where("created_at > ?", most_recent_date - 1.minutes).where(spot: self)
+    else
+      fetch_and_parse_forecast_data
+      # code ci dessous pas DRY, on pourrait tenter un truc récursif en rappelant la fonction forecast_data
+      self.forecasts.each do |forecast|
+        most_recent_date = forecast.created_at if forecast.created_at > most_recent_date
+      end
+      @fresh_forecasts = Forecast.where("created_at > ?", most_recent_date - 2.minutes).where(spot: self)
+      # fin du pas très DRY
+    end
+    @fresh_forecasts
+  end
+
+  def fetch_and_parse_forecast_data
+    # Calling darksky api and saving in DB
+    lat = self.lat
+    lng = self.lng
+    url = open("https://api.darksky.net/forecast/#{ENV['DARKSKY_API_KEY']}/#{lat},#{lng}?units=ca").read
+    forecast_raw_json = JSON.parse(url)
+
+    hourly = forecast_raw_json["hourly"]["data"]
+
+    hourly.each do |data_for_the_hour|
+      forecast = Forecast.new({
+        spot: self,
+        date_time: DateTime.strptime(data_for_the_hour["time"].to_s, '%s'),
+        wind_strength: (data_for_the_hour["windSpeed"]/1.852), # vitesse du vent enregistrée en kt
+        wind_direction: data_for_the_hour["windBearing"],
+        precip_intensity: data_for_the_hour["precipIntensity"],
+        precip_probability: data_for_the_hour["precipProbability"],
+        cloud_cover: data_for_the_hour["cloudCover"],
+        temperature: data_for_the_hour["temperature"]
+      })
+      forecast.save
+    end
+  end
 end
+
