409f7baef0ff8b6f4fbffba084cd73de9b77c106
Jerome BURKARD <jerome.burkard@gadz.org>
2016-11-29 11:28:33 +0100

    forecast validation updated

--- app/models/forecast.rb
+++ app/models/forecast.rb
@@ -1,6 +1,6 @@
 class Forecast < ApplicationRecord
   belongs_to :spot
-  validates :date_time
+  validates :date_time, presence: true
   validates :wind_direction, presence: true, numericality: { only_integer: true }, inclusion: { in: (0..360).to_a }
   validates :wind_strength, presence: true, numericality: true
   validates :wind_gusting, presence: true, numericality: true
