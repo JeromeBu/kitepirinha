7a55d3daca6f00bdcd3348ff0ed4126ba06b7f55
Romain Richard <romain.g.richard@gmail.com>
2016-12-01 10:35:41 +0100

    work in progress

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161129222256) do
+ActiveRecord::Schema.define(version: 20161130180245) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -100,6 +100,7 @@ ActiveRecord::Schema.define(version: 20161129222256) do
     t.boolean  "accepted"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "address"
     t.index ["harbor_id"], name: "index_spots_on_harbor_id", using: :btree
     t.index ["user_id"], name: "index_spots_on_user_id", using: :btree
   end
