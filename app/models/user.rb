ff6408c93b0f82fb385c3b87abefce7b1a861a5d
Julie Pierre <julie.pierre3@gmail.com>
2016-12-01 16:26:13 +0100

    completed facebook connect

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,31 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: [:facebook]
   has_many :reviews
   has_many :weather_feedbacks
   has_many :spots
   has_many :favorite_spots
 
   validates :weight, numericality: { only_integer: true }, allow_blank: true
+
+  def self.find_for_facebook_oauth(auth)
+    user_params = auth.to_h.slice(:provider, :uid)
+    user_params.merge! auth.info.slice(:email, :first_name, :last_name)
+    user_params[:facebook_picture_url] = auth.info.image
+    user_params[:token] = auth.credentials.token
+    user_params[:token_expiry] = Time.at(auth.credentials.expires_at)
+
+    user = User.where(provider: auth.provider, uid: auth.uid).first
+    user ||= User.where(email: auth.info.email).first # User did a regular sign up in the past.
+    if user
+      user.update(user_params)
+    else
+      user = User.new(user_params)
+      user.password = Devise.friendly_token[0,20]  # Fake password for validation
+      user.save
+    end
+
+    return user
+  end
 end
