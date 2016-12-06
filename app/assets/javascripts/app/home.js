f39bf29c75d77a6a17501951b11dc2bec75a68b0
Romain Richard <romain.g.richard@gmail.com>
2016-12-06 09:39:28 +0100

    trying to solve bug

--- app/assets/javascripts/app/home.js
+++ app/assets/javascripts/app/home.js
@@ -1,1 +1,7 @@
-// JS for wind icon?
+$('.kite-home-btn').on('click', function(event) {
+  event.preventDefault();
+  $('#main-title').toggleClass('hidden');
+  $('#search').toggleClass('hidden');
+  $('.wing_size').toggleClass('hidden');
+  $('.home_sea').toggleClass('hidden');
+});
--- app/assets/javascripts/home_button_event.js
+++
@@ -1,8 +0,0 @@
-
-$('.kite-home-btn').on('click', function(event) {
-  event.preventDefault();
-  $('#main-title').toggleClass('hidden');
-  $('#search').toggleClass('hidden');
-  $('.wing_size').toggleClass('hidden');
-  $('.home_sea').toggleClass('hidden');
-});
