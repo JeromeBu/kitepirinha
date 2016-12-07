9a68d24c7f6f370931c783d559b71a7a3992ae06
Romain Richard <romain.g.richard@gmail.com>
2016-12-07 16:07:08 +0100

    WIP navbar and search

--- app/assets/javascripts/app/maps_navbar.js
+++ app/assets/javascripts/app/maps_navbar.js
@@ -1,13 +1,13 @@
 $(window).scroll(function () {
   // $('.width_box').css('top', '115px')
 
-  if ($(window).scrollTop() < 65) {
+  if ($(window).scrollTop() < 80) {
     var topPage = $(window).scrollTop();
-    var setTop = 120 - topPage;
+    var setTop = 135 - topPage;
     $('.width_box').css('top', setTop + 'px')
-    $('#map').css('height', 'calc(100vh - 120px')
+    $('#map').css('height', 'calc(100vh - 135px')
   }
-  if ($(window).scrollTop() > 65) {
+  if ($(window).scrollTop() > 80) {
     $('.width_box').css('top', '55px')
     $('#map').css('height', 'calc(100vh - 55px')
   }
