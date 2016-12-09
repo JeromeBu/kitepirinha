09f2af5410d6830f47e6fccb6fae39ff5e1b60e8
Julie Pierre <julie.pierre3@gmail.com>
2016-12-09 11:07:42 +0100

    finished

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -97,7 +97,7 @@ class Spot < ApplicationRecord
       mean_direction = (Math.atan2(mean_y_direction, mean_x_direction) * 180 / Math::PI).round
       mean_direction = mean_direction + 360 if mean_direction < 0
       mean_feedback = {
-        message: "#{last_feedbacks.length} feedbacks sur les 2 dernières heures, le dernier à #{last_feedback.created_at + 1.hour).strftime('%H:%M')}",
+        message: "#{last_feedbacks.length} infos sur les 2 dernières heures, la dernière à #{(last_feedback.created_at + 1.hour).strftime('%H:%M')}",
         mean_strength: mean_strength,
         mean_direction: mean_direction,
         max_strength: max_strength,
