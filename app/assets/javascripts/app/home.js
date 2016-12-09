682b4ad3c9aa133cabfea727356650e64ffe6cc2
Romain Richard <romain.g.richard@gmail.com>
2016-12-09 12:14:03 +0100

    home.js done

--- app/assets/javascripts/app/home.js
+++ app/assets/javascripts/app/home.js
@@ -1,20 +1,39 @@
-  $('#first-button').on('click', function(event) {
-    event.preventDefault();
+$('#first-button').on('click', function(event) {
+  event.preventDefault();
 
-    $('#first-button').toggleClass('hidden');
+  $('#first-button').toggleClass('hidden');
 
-    $('#main-title').toggleClass('hidden');
-    $('.search_address_box').toggleClass('hidden');
+  $('#main-title').toggleClass('hidden');
+  $('.search_address_box').toggleClass('hidden');
 
-    $('.home_sea').toggleClass('hidden');
-  });
+  $('.home_sea').toggleClass('hidden');
+});
 
-  $('#arrow').on('click', function(event) {
-    $('.wing_size').toggleClass('hidden');
-    setTimeout(function (){
-      $('.wing_size').toggleClass('active');
-    }, 10);
-    setTimeout(function (){
-      $('#arrow').toggleClass('pointer-disable');
-    }, 1);
-  });
+$('#arrow').on('click', function(event) {
+  $('.wing_size').toggleClass('hidden');
+  setTimeout(function (){
+    $('.wing_size').toggleClass('active');
+  }, 10);
+  setTimeout(function (){
+    $('#arrow').toggleClass('pointer-disable');
+  }, 1);
+});
+
+var count = 0
+$('input').keyup(function(event) {
+  if (event.keyCode === 13) {
+    if (count < 1) {
+      count += 1;
+      console.log(count)
+      $('.wing_size').toggleClass('hidden');
+      setTimeout(function (){
+        $('.wing_size').toggleClass('active');
+      }, 10);
+      setTimeout(function (){
+        $('#arrow').toggleClass('pointer-disable');
+      }, 1);
+    } if (count > 1) {
+      console.log("hello")
+    }
+  }
+});
